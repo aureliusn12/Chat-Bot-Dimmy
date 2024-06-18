637bfdccf4c0650ae9d48bf5bae5e241ea559761
aureliusn12 <163034788+aureliusn12@users.noreply.github.com>
2024-06-18 12:14:55 -0300

    alguns problemas descorbertos e solucionados

--- ChatBot.py
+++ ChatBot.py
@@ -19,21 +19,21 @@ def find_best_match(user_question: str, questions: list[str]) -> str | None:
 
 
 def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
-    for q in knowledge_base["questions"]:
-        if q["question"] == question:
-            return q["answer"]
+    for q in knowledge_base["perguntas"]:
+        if q["pergunta"] == question:
+            return q["resposta"]
         
         
 def chat_bot():
     knowledge_base: dict = load_knowledge_base('knowledge_base.json')
     
     while True:
-        user_input: str = input('Voce:')
+        user_input: str = input('Voce: ')
         
         if user_input.lower() == 'sair':
             break
         
-        best_match: str | None = find_best_match(user_input, [q["question"]for q in knowledge_base["questions"]])
+        best_match: str | None = find_best_match(user_input, [q["pergunta"]for q in knowledge_base["perguntas"]])
         
         if best_match:
              answer: str = get_answer_for_question(best_match, knowledge_base)
@@ -43,7 +43,7 @@ def chat_bot():
             new_answer: str = input('Digite a resposta ou digite "pular" para pular: ')
             
             if new_answer.lower() != 'pular':
-                knowledge_base["questions"].append({"question": user_input, "answer":new_answer})
+                knowledge_base["perguntas"].append({"pergunta": user_input,"resposta":new_answer})
                 save_knowledge_base('knowledge_base.json', knowledge_base)
                 print('Bot: Obrigado! Eu aprendi uma nova resposta!')
                 
